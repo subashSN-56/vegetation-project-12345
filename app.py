3804be0b100601c6e2481640a68335102c361915
subashSN-56 <subash852488@gmail.com>
2026-03-31 11:42:03 +0530

    qwert

--- app.py
+++ app.py
@@ -78,6 +78,7 @@ def index():
     return render_template("index.html", uploaded_image=None, output_image=None)
 
 
-# Run app
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
